d66d48ee41be192e721ba1b8afc669df0189b487
Mckomo <mckomo@gmail.com>
2016-01-10 16:36:38 +0100

    Register stylesheet paths with SASS

--- lib/bootswatch-rails.rb
+++ lib/bootswatch-rails.rb
@@ -3,6 +3,19 @@ require 'bootswatch-rails/version'
 module Bootswatch
   module Rails
     class Engine < ::Rails::Engine
+
+      initializer 'bootswatch-rails.assets.append_paths' do |app|
+        ::Sass.load_paths << stylesheets_path if sass?
+      end
+
+      def stylesheets_path
+        root.join('vendor', 'assets', 'stylesheets').to_s
+      end
+
+      def sass?
+        defined?(::Sass)
+      end
+
     end
   end
 end
